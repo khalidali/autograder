590bcf62510be64d42693734ffccffc04208bd11
Omar Ali <omarali@omarali.us>
2012-03-16 22:58:46 -0700

    Created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autograder::Application.config.secret_token = '629e6b8f6d4da37953cc70d6052c9214ce29527985638b888f136c51761b9c27d5f2d0a95a93c8e538697ad6cc31861870b931719fb06d3b35df3553143abe76'
