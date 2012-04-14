ae1733130832a07860aaa22607f713bfab9bc7a5
Robert Gasparyan <saasbook@saasbook.(none)>
2012-04-14 01:50:14 -0700

    autograding works with tmp/local files

--- init_script1.sh
+++ init_script1.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 curl -X POST -d "prof_key=armando&student_keys=[robert, khalid, ernest]" localhost:3000/assignments/create.json
 curl -X PUT -F autograder=@inst_autograder.rb localhost:3000/assignments/1/add_autograder.json
 curl -X PUT localhost:3000/assignments/1/submit.json -F submission=@student_code.rb -F "student_key=robert"
