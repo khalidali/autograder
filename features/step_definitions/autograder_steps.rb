5367b6103126fc1f057569507476e143b61fab46
Omar Ali <github@omarali.us>
2012-04-22 16:56:12 -0700

    added pending cucumber steps

--- features/step_definitions/autograder_steps.rb
+++ features/step_definitions/autograder_steps.rb
@@ -2,3 +2,32 @@
 Given /^an assignment with id "([^"]*)" exists$/ do |arg1|
   Assignment.create :id => arg1
 end
+
+Given /^the test db is populated$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I allow the following keys "([^"]*)" to submit to the assignment whose id is "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I upload the grading file "([^"]*)" to the assignment whose id is "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^"([^"]*)" submits$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I retrieve all submissions to the assignment whose id is "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I retrieve all pending submissions to the assignment whose id is "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I upload the autograder "([^"]*)" to the assignment whose id is "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
