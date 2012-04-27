4491ff0b852c71203c8f6faf9599025192ba7017
Robert Gasparyan <saasbook@saasbook.(none)>
2012-04-26 23:05:20 -0700

    refactored grading code into resque worker

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,9 +1,3 @@
 class Submission < ActiveRecord::Base
   belongs_to :student
-  
-  def add_to_queue
-
-    system("./node.rb #{self.id} '#{self.student.assignment.autograder}' '#{self.body}'")
-  end
-  
 end
