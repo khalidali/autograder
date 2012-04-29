44f63b8beaca6d9cfd9f47b1bf88eb6544346bd8
Robert Gasparyan <saasbook@saasbook.(none)>
2012-04-29 16:31:31 -0700

    removed update status

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,8 +1,2 @@
 class SubmissionsController < ApplicationController
-  def update_status
-    @submission = Submission.find_by_id(params[:id])
-    @submission.output = File.open(params[:output].tempfile.path, "rb").read() 
-    @submission.status = params[:status] 
-    @submission.save
-  end
 end
