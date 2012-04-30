17a0f67465c81c91ba8cff5bbea7fa64fe87908a
Ubuntu <ubuntu@domU-12-31-39-0E-E5-8E.compute-1.internal>
2012-04-30 02:25:57 +0000

    assignment rspec completed

--- spec/controllers/instructors_controller_spec.rb
+++ spec/controllers/instructors_controller_spec.rb
@@ -3,13 +3,28 @@ require 'spec_helper'
 describe InstructorsController do
 
   describe "GET 'index'" do
+		it "should render error if no keys were given" do
+      get 'status', :format => :json
+      response.should render_template(:text => 'required param \'keys\' missing.')
+    end
     it "returns http success" do
       get 'index', :format => :json
       response.should be_success
     end
   end
 
+	describe "GET 'status'" do
+    it "should render error if no keys were given" do
+      get 'status', :format => :json
+      response.should render_template(:text => 'required param \'keys\' missing.')
+    end
+  end
+
   describe "GET 'authorize'" do
+		it "should render error if no keys were given" do
+      get 'status', :format => :json
+      response.should render_template(:text => 'required param \'keys\' missing.')
+    end
     it "returns http success" do
       get 'authorize', :format => :json
       response.should be_success
@@ -17,6 +32,10 @@ describe InstructorsController do
   end
 
   describe "GET 'deauthorize'" do
+		it "should render error if no keys were given" do
+      get 'status', :format => :json
+      response.should render_template(:text => 'required param \'keys\' missing.')
+    end
     it "returns http success" do
       get 'deauthorize', :format => :json
       response.should be_success
