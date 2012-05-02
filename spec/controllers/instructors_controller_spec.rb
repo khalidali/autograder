14c61d97f286d0055223cd391dcc1949519a7ed0
Robert Gasparyan <saasbook@saasbook.(none)>
2012-05-02 02:01:40 -0700

    Instructor rspec done

--- spec/controllers/instructors_controller_spec.rb
+++ spec/controllers/instructors_controller_spec.rb
@@ -4,51 +4,51 @@ describe InstructorsController do
 
   describe "GET 'index'" do
     it "should render error if no keys were given" do
-      get 'index', :format => :json
+      get 'index', :format => :json, :super_key => "1234567890"
       response.should render_template(:text => 'required param \'keys\' missing.')
     end
     it "returns http success" do
-      get 'index', :format => :json
+      get 'index', :format => :json, :super_key => "1234567890"
       response.should be_success
     end
   end
 
   describe "GET 'status'" do
 		it "should render error if no keys were given" do
-      get :status, :format => :json
+      get :status, :format => :json, :super_key => "1234567890"
       response.should render_template(:text => 'required param \'keys\' missing.')
     end
     it "returns http success" do
 			Instructor.should_receive(:find_by_key).with('robert').and_return(true)
 			Instructor.should_receive(:find_by_key).with('khalid').and_return(true)
-      get :status, :keys => "[khalid, robert]", :format => :json
+      get :status, :keys => "[khalid, robert]", :format => :json, :super_key => "1234567890"
     end
   end
 
   describe "GET 'authorize'" do
 		it "should render error if no keys were given" do
-      get 'status', :format => :json
+      get 'status', :format => :json, :super_key => "1234567890"
       response.should render_template(:text => 'required param \'keys\' missing.')
     end
     it "returns http success" do
-      get 'authorize', :format => :json
+      get 'authorize', :format => :json, :super_key => "1234567890"
       response.should be_success
     end
     it "should authorize student keys" do
       Instructor.should_receive(:find_by_key).with('robert').and_return(false)
 			Instructor.should_receive(:find_by_key).with('khalid').and_return(true)
 			Instructor.should_receive(:create!)
-      get 'authorize', :format => :json, :keys => "[khalid, robert]"
+      get 'authorize', :format => :json, :keys => "[khalid, robert]", :super_key => "1234567890"
     end
   end
 
   describe "GET 'deauthorize'" do
 		it "should render error if no keys were given" do
-      get 'status', :format => :json
+      get 'status', :format => :json, :super_key => "1234567890"
       response.should render_template(:text => 'required param \'keys\' missing.')
     end
     it "returns http success" do
-      get 'deauthorize', :format => :json
+      get 'deauthorize', :format => :json, :super_key => "1234567890"
       response.should be_success
     end
     it "should deauthorize instructor keys" do
@@ -56,8 +56,13 @@ describe InstructorsController do
       Instructor.should_receive(:find_by_key).with('robert').and_return(@instructor1)
 			Instructor.should_receive(:find_by_key).with('khalid').and_return(nil)
 			@instructor1.should_receive(:delete)
-			get 'deauthorize', :format => :json, :keys => "[khalid, robert]"
+			get 'deauthorize', :format => :json, :keys => "[khalid, robert]",:super_key => "1234567890"
 	  end
-
   end
+  describe "GET 'index' with invalid superkey" do
+    it "should render error if no keys were given" do
+      get 'index', :format => :json, :super_key => "12345"
+      response.should render_template(:text => 'SuperUser permission required for this action')
+    end
+  end    
 end
