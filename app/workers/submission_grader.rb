637fbcbe24e06a2307de3a86b96b2a036337b142
EAMA <lm2914ernest@yahoo.com.hk>
2012-05-02 16:47:24 -0700

    fix submission_grader

--- app/workers/submission_grader.rb
+++ app/workers/submission_grader.rb
@@ -6,21 +6,14 @@ class SubmissionGrader
   def self.perform(submission_id)
     submission = Submission.find(submission_id)
 
-    puts "AUTOGRADER::WORKER ID    : " + submission_id.to_s
-    puts "AUTOGRADER::WORKER GRADER: " + submission.student.assignment.autograder
-    puts "AUTOGRADER::WORKER SUBMIS: " + submission.body
+    #puts "AUTOGRADER::WORKER ID    : " + submission_id.to_s
+    #puts "AUTOGRADER::WORKER GRADER: " + submission.student.assignment.autograder
+    #puts "AUTOGRADER::WORKER SUBMIS: " + submission.body
 
-    autograder = Tempfile.new("tmp_autograder")
-    autograder.write(submission.student.assignment.autograder)
-    autograder.chmod(0777)
-    autograder.close
+    autograder = Tempfile.new("tmp_autograder").write(submission.student.assignment.autograder).chmod(0777).close
+    student_code  = Tempfile.new("tmp_student_code").write(submission.body).chmod(0777).close
 
-    student_code  = Tempfile.new("tmp_student_code")
-    student_code.write(submission.body)
-    student_code.chmod(0777)
-    student_code.close
-
-    puts "AUTOGRADER::WORKER now I'm gonna run"
+    #puts "AUTOGRADER::WORKER now I'm gonna run"
     
     submission.output = `#{autograder.path} #{student_code.path}`
     submission.status = 'completed'
